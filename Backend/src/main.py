7c7f83d5f1b07b68e347544d7a853558810acad2
david <david.aryasetia@gmail.com>
2026-01-30 09:35:27 +0700

    Refactoring Code : - refactoring .env.example - test CI/CD

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -7,7 +7,7 @@ from src import database_models
 from sqlalchemy.orm import Session
 
 app = FastAPI(
-    title="Warung Backend",
+    title="Warungku Backend Test CI/CD",
     description="Playground with Fast-API Framework, and Devops CI/CD to develop simple warungku backend",
     version="V.1"
 )
